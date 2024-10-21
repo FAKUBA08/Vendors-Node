78bcc2b1aa98eacf394bd6428375b2a9ca084c12
FAKUBA08 <badmusfaruq01@gmail.com>
2024-10-21 10:47:23 +0100

    All

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // app.js
 const express = require("express");
 const authRoutes = require("./routes/auth");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 
-const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
-
-
-
 dotenv.config();
-const app = express();
-
-
-app.use(express.json());
-const cors = require('cors');
-
-app.use(cors());
-
+const app = require("./app")
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
 mongoose.connect(process.env.MONGO_URI,)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error: ", err));
 
 
-app.use('/api/auth', authRoutes);
 
 
 const PORT = process.env.PORT || 3000;
